371bcc4fddbd84b1b077300a4668b1f47828055e
William Roger <williamroger.frontend@gmail.com>
2021-02-19 03:56:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
+import React, { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handlePlus() {
+    setTimeout(() => {
+      setCounter((prevState) => prevState + 1);
+    }, 2000);
+  }
+
+  function handleMinus() {
+    setTimeout(() => {
+      setCounter((prevState) => prevState - 1);
+    }, 2000);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+      <button onClick={handleMinus}>-</button>
+      <button onClick={handlePlus}>+</button>
     </div>
   );
 }
